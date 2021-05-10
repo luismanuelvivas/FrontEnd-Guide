a5acdc4ac2af0bfcc015fc60e41d4d0f52fc81ee
Luis Manuel Vivas <luis.manuel.vivas.r@gmail.com>
2021-05-10 15:12:14 -0400

    script for content started

--- scripts/index.js
+++ scripts/index.js
@@ -10,3 +10,10 @@ buttonDown.onclick = () => {
 buttonUp.onclick = () => {
     header.className = 'header'
 }
+
+//CONTENT ANIMATION
+
+const section1 = document.querySelector('.section1')
+const section1Position = section1.getBoundingClientRect().top
+
+console.log(section1Position)
