18b25093b9a9bc631034b21c27a4c12746b1494a
Luis Manuel Vivas <luis.manuel.vivas.r@gmail.com>
2021-05-10 12:10:53 -0400

    header animation

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+//HEADER ANIMATION
+const buttonDown = document.querySelector('#down-arrow')
+const buttonUp = document.querySelector('#up-arrow')
+const header = document.querySelector('header')
+
+buttonDown.onclick = () => {
+    header.className = 'header-black'
+}
+
+buttonUp.onclick = () => {
+    header.className = 'header'
+}
